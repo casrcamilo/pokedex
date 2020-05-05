037455d975fb4dcb85804d60c1e6a41874e862ec
Camilo Sosa <camilososa@10.0.2.15>
2020-05-04 19:02:53 -0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Camilo Sosa on 5/4/20.
+//  Copyright © 2020 Camilo Sosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
